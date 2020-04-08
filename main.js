cf1af6c267ec9885d8b0c05762ab0ef8eba53bbf
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-04-09 00:47:17 +0530

    cart event

---
+++ main.js
@@ -0,0 +1,123 @@
+var removeCartButtons = document.getElementsByClassName('btn-danger');
+// console.log(removeCartButtons);	
+
+for(let i=0;i<removeCartButtons.length;i++){
+	var button = removeCartButtons[i];
+	button.addEventListener('click',removeCartItem);
+}
+
+var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+for(let i=0;i<quantityInputs.length;i++){
+	var input = quantityInputs[i];
+	input.addEventListener('change',quantityChanged);
+}
+
+var addToCartButtons = document.getElementsByClassName('shop-item-button');
+for(let i=0;i<addToCartButtons.length;i++){
+	var button = addToCartButtons[i];
+	button.addEventListener('click',addToCartClicked);
+}
+
+document.getElementsByClassName('btn-purchase')[0].addEventListener('click',purchaseClicked);
+
+function purchaseClicked(event) {
+	alert('Thank you for shopping with us! - Chris Martin');
+
+	var cartItems = document.getElementsByClassName('cart-items')[0];
+
+	while(cartItems.hasChildNodes()){
+		cartItems.removeChild(cartItems.firstChild);
+	}
+	updateCartTotal();
+}
+
+function removeCartItem(event) {
+	buttonClicked = event.target;
+	buttonClicked.parentElement.parentElement.remove();
+	updateCartTotal();
+}
+
+function quantityChanged(event) {
+	
+	var input = event.target
+	if(isNaN(input.value) || input.value <= 0){
+		input.value = 1;
+	}
+	updateCartTotal();
+}
+
+function addToCartClicked(event) {
+	var button = event.target;
+	var shopItem = button.parentElement.parentElement;
+
+	var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+	// console.log(title);
+
+	var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+	var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src;
+
+	// console.log(title,price,imageSrc);
+	addItemToCart(title,price,imageSrc);
+	updateCartTotal();
+}
+
+function addItemToCart(title,price,imageSrc) {
+	var cartRow = document.createElement('div');
+	// cartRow.innerText = title;
+	cartRow.classList.add('cart-row')  // ?
+	var cartItems = document.getElementsByClassName('cart-items')[0];
+
+	var cartItemsNames = cartItems.getElementsByClassName('cart-item-title');
+		
+	for (var i = cartItemsNames.length - 1; i >= 0; i--) {
+		if(cartItemsNames[i].innerText==title){
+			alert('This item is already added to the cart');
+			return;
+		}
+	}
+
+	var cartRowContents = `
+					<div class="cart-item cart-column">
+		                <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+		                <span class="cart-item-title">${title}</span>
+		            </div>
+
+	                <span class="cart-price cart-column">${price}</span>
+
+	                <div class="cart-quantity cart-column">
+		                <input class="cart-quantity-input" type="number" value="1">
+		                <button class="btn btn-danger" type="button">REMOVE</button>
+		            </div>`
+    
+    cartRow.innerHTML = cartRowContents;
+	cartItems.append(cartRow);
+
+	cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click',removeCartItem);
+
+	cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change',quantityChanged);
+}
+
+function updateCartTotal() {
+	var carItemContainer = document.getElementsByClassName('cart-items')[0];
+	var cartRows = carItemContainer.getElementsByClassName('cart-row');
+
+	var total = 0;
+
+	for(let i=0;i<cartRows.length;i++){
+		var cartRow = cartRows[i];
+		var priceElemnt = cartRow.getElementsByClassName('cart-price')[0];
+
+		var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+
+		// console.log(priceElemnt,quantityElement);
+		var price = parseFloat(priceElemnt.innerText.replace('$',''));
+		var quantity = quantityElement.value;
+
+		// console.log(price*quantity);
+		total+= (price*quantity);
+	}
+
+	total= Math.round(total*100)/100;
+
+	document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
+}
